d6376ed4e6c013cb3a1a6a490f85bb71151527ca
Juyeong Noh <juyeongnoh@gmail.com>
2025-11-13 14:51:56 +0900

    feat: enhance branch management with validation and merge checks

--- porcelains/branch.js
+++ porcelains/branch.js
@@ -6,17 +6,19 @@ import {
   writeFileSync,
 } from "node:fs";
 import { join } from "node:path";
+import Commit from "../types/Commit.js";
 
 const gitDir = join(process.cwd(), ".git");
 
 function branch(flag, args) {
+  const branchName = args[0];
+
   switch (flag) {
     case undefined:
       listBranches();
       break;
     // 브랜치 생성
     case "-c":
-      const branchName = args[0];
       createBranch(branchName);
       break;
     // 브랜치 이름 변경
@@ -29,19 +31,103 @@ function branch(flag, args) {
         console.log("오류: 현재 어떤 브랜치에도 속해 있지 않습니다.");
         return;
       }
-      const newBranchName = args[0];
-      changeBranchName(currentBranchName, newBranchName);
+      changeBranchName(currentBranchName, branchName);
       break;
     // 브랜치 삭제
     case "-d":
-      // 삭제 전 확인 로직
-      const delBranchName = args[0];
-      deleteBranch(delBranchName);
+      if (!isValidBranchName(branchName)) {
+        console.log("오류: 브랜치명을 찾을 수 없습니다.");
+        return;
+      }
+
+      if (isCurrentBranch(branchName)) {
+        console.log("오류: 현재 브랜치를 삭제할 수 없습니다.");
+        return;
+      }
+
+      if (!checkBranchMerged(branchName)) {
+        console.log("오류: 브랜치가 병합되지 않았습니다.");
+        return;
+      }
+
+      deleteBranch(branchName);
       break;
     default:
       console.log(`오류: 알 수 없는 플래그 '${flag}' 입니다.`);
       return;
   }
+}
+
+function isValidBranchName(branchName) {
+  const headsDir = join(gitDir, "refs", "heads");
+  const branches = readdirSync(headsDir);
+
+  return branches.includes(branchName);
+}
+
+function checkBranchMerged(branchName) {
+  const branchHeadDir = join(gitDir, "refs", "heads", branchName);
+  const branchHead = readFileSync(branchHeadDir).toString().trim();
+
+  const headDir = join(gitDir, "HEAD");
+  const head = readFileSync(headDir).toString().trim();
+  let currentBranch;
+
+  if (head.startsWith("ref:")) {
+    currentBranch = head.split("/").pop();
+  } else {
+    return true;
+  }
+
+  const currentBranchHeadDir = join(gitDir, "refs", "heads", currentBranch);
+  const currentBranchHead = readFileSync(currentBranchHeadDir)
+    .toString()
+    .trim();
+
+  const currentBranchHeadObjectDir = join(
+    gitDir,
+    "objects",
+    currentBranchHead.slice(0, 2),
+    currentBranchHead.slice(2)
+  );
+
+  const currentCommit = Commit.fromObject(
+    readFileSync(currentBranchHeadObjectDir)
+  );
+
+  if (traverseCommits(currentBranchHead, branchHead)) {
+    return true;
+  }
+
+  return false;
+}
+
+function traverseCommits(commitHash, targetHash, visited = new Set()) {
+  if (commitHash === targetHash) {
+    return true;
+  }
+
+  if (visited.has(commitHash)) {
+    return false;
+  }
+
+  visited.add(commitHash);
+
+  const commitObjectDir = join(
+    gitDir,
+    "objects",
+    commitHash.slice(0, 2),
+    commitHash.slice(2)
+  );
+
+  const commit = Commit.fromObject(readFileSync(commitObjectDir));
+  for (const parentHash of commit.parents) {
+    if (traverseCommits(parentHash, targetHash, visited)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 function listBranches() {
@@ -63,24 +149,23 @@ function listBranches() {
   });
 }
 
-/**
- * @todo unmerge된 커밋이 있는지 확인하는 로직 추가하기
- */
-function deleteBranch(branchName) {
+function isCurrentBranch(branchName) {
   const currentHead = readFileSync(join(gitDir, "HEAD")).toString().trim();
 
   let currentBranchName;
+
   if (currentHead.startsWith("ref:")) {
     currentBranchName = currentHead.split("/").pop();
   }
 
   if (branchName === currentBranchName) {
-    console.log(
-      `오류: 현재 '${branchName}' 브랜치에 있습니다. 해당 브랜치를 삭제할 수 없습니다.`
-    );
-    return;
+    return true;
   }
 
+  return false;
+}
+
+function deleteBranch(branchName) {
   try {
     rmSync(join(gitDir, `refs/heads/${branchName}`));
   } catch {
--- types/Commit.js
+++ types/Commit.js
@@ -1,3 +1,4 @@
+import { inflateSync } from "zlib";
 import { hashObject } from "../plumbings/hashObject.js";
 
 class Commit {
@@ -9,6 +10,68 @@ class Commit {
     this.message = message;
   }
 
+  /**
+   * @param {Buffer} commit
+   */
+  static fromObject(commit) {
+    const unzippedObjectBuffer = inflateSync(commit);
+
+    let offset = 0;
+    let header = "";
+
+    while (unzippedObjectBuffer[offset] !== 0) {
+      header += String.fromCharCode(unzippedObjectBuffer[offset]);
+      offset++;
+    }
+    offset++;
+
+    const [type, sizeStr] = header.split(" ");
+    const size = parseInt(sizeStr, 10);
+
+    if (type !== "commit" || size !== unzippedObjectBuffer.length - offset) {
+      throw new Error("Invalid commit object");
+    }
+
+    let content = unzippedObjectBuffer.reduce((acc, byte, idx) => {
+      if (idx >= offset) {
+        acc += String.fromCharCode(byte);
+      }
+      return acc;
+    }, "");
+    const lines = content.split("\n");
+
+    let tree = "";
+    const parents = [];
+    let author = "";
+    let committer = "";
+    let messageLines = [];
+    let i = 0;
+
+    for (; i < lines.length; i++) {
+      const line = lines[i];
+      if (line === "") {
+        break;
+      }
+
+      const [key, ...rest] = line.split(" ");
+      const value = rest.join(" ");
+
+      if (key === "tree") {
+        tree = value;
+      } else if (key === "parent") {
+        parents.push(value);
+      } else if (key === "author") {
+        author = value;
+      } else if (key === "committer") {
+        committer = value;
+      }
+    }
+
+    messageLines = lines.slice(i + 1);
+    const message = messageLines.join("\n");
+
+    return new Commit(tree, parents, author, committer, message);
+  }
   getContent() {
     let content = `tree ${this.tree}\n`;
     this.parents.forEach((parent) => {
